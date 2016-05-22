0045129185e68c5b0df2bf5e49b58f4f77143b39
ryans610 <ryans610@gmail.com>
2016-05-22 23:38:18 +0800

    add board control

--- surakarta.js
+++ surakarta.js
@@ -1,1 +1,51 @@
 /*Surakarta Control*/
+//Class Chess
+var Chess={
+    //constructor
+    newChess:function(x,y){
+        var chess={};
+        //variable
+        chess.chess=document.getElementById("chess-x"+x+"-y"+y);
+        chess.color=0;
+        //method
+        chess.clear=function(){
+            this.chess.classList.remove("has-chess","prompt","black","white");
+            this.color=0;
+        };
+        chess.prompt=function(){
+            this.chess.classList.add("prompt");
+        };
+        chess.notPrompt=function(){
+            this.chess.classList.remove("prompt");
+        };
+        chess.setChess=function(color){
+            this.chess.classList.add("has-chess",Chess.colors[color]);
+            this.color=color;
+        };
+        //return
+        return chess;
+    },
+    //static variable
+    colors:[
+        null,
+        "black",
+        "white",
+    ],
+};
+
+//Class Board
+var Board={
+    //variable
+    board:[],
+    //method
+    init:function(){
+        this.board.length=0;
+        
+    },
+};
+
+/*
+for(var i=0;i<6;i++) {
+    board.push([]);
+}
+*/

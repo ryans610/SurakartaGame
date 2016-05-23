0ac71cad4c1b3262ed7b9e281bfc530c06860175
ryans610 <ryans610@gmail.com>
2016-05-23 11:21:30 +0800

    update Board method

--- surakarta.js
+++ surakarta.js
@@ -7,6 +7,8 @@ var Chess={
         //variable
         chess.chess=document.getElementById("chess-x"+x+"-y"+y);
         chess.color=0;
+        chess.x=x;
+        chess.y=y;
         //method
         chess.clear=function(){
             this.chess.classList.remove("has-chess","prompt","black","white");
@@ -27,25 +29,59 @@ var Chess={
     },
     //static variable
     colors:[
-        null,
+        undefined,
         "black",
         "white",
     ],
 };
 
-//Class Board
+//Board
 var Board={
     //variable
     board:[],
+    chess:[undefined,[],[]],
     //method
     init:function(){
+        //clear
         this.board.length=0;
-        
+        this.chess[1].length=0;
+        this.chess[2].length=0;
+        //init
+        for(var i=0;i<=5;i++){
+            this.board.push([]);
+            for(var j=0;j<=5;j++){
+                this.board[i][j]=Chess.newChess(i,j);
+                this.board[i][j].clear();
+            }
+        }
+        for(var i=0;i<=5;i++){
+            for(var j=0;j<=1;j++){
+                this.board[i][j].setChess(1);
+                this.chess[1].push(this.board[i][j]);
+            }
+            for(var j=4;j<=5;j++){
+                this.board[i][j].setChess(2);
+                this.chess[2].push(this.board[i][j]);
+            }
+        }
+    },
+    hasChess:function(x,y){
+        return board[x][y].color;
+    },
+    moveChess:function(x1,y1,x2,y2){
+        var a=this.board[x1][y1];
+        var b=this.board[x2][y2];
+        var index=-1;
+        for(var i=0;i<this.chess[a.color].length;i++){
+            if(this.chess[a.color][i].x==x1&&this.chess[a.color][i].y==y1){
+                index=i;
+                break;
+            }
+        }
+        b.setChess(a.color);
+        this.chess[a.color][index]=b;
+        a.clear();
+    },
+    eatChess:function(x1,y1,x2,y2){
     },
 };
-
-/*
-for(var i=0;i<6;i++) {
-    board.push([]);
-}
-*/
